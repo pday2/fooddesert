dd5c42a23baaadf65f5d089263772635ec1d8075
Peter <pday2@uwyo.edu>
2022-06-03 09:38:41 +0200

    cleaned health and grocery data

---
+++ grocery/cleanGrocery.R
@@ -0,0 +1,25 @@
+library("tidyverse")
+if (Sys.info()[1] == "Windows") {
+  setwd("C:/Users/peter/My Tresors/Documentsacer/KULeuven/CollAnaBigData/data/grocery")
+} else {
+  setwd("/home/muddy/Tresors/Documentsacer/KULeuven/CollAnaBigData/data/grocery")   
+}
+# https://www.openicpsr.org/openicpsr/project/123042/version/V1/view
+# National Neighborhood Data Archive (NaNDA): 
+#          Grocery Stores by ZIP Code Tabulation Area, United States, 2003-2017
+groceryFile <- 'nanda_grocery_zcta_2003-2017_01P.csv'
+grocery <- read.csv(groceryFile)
+
+grocery2017 <- grocery %>% filter(year==2017)
+
+sum(is.na(grocery2017$population)) # = 142
+sum(is.na(grocery2017$popden_445110)) # = 421
+
+grocery2017Filter = grocery2017 %>% filter( !is.na(population) |  !is.na(popden_445110))
+
+groceryData = grocery2017Filter[c('zcta19', 'year', 'population', 'aland10', 
+                                  'count_sales_445110', 'popden_sales_445110', 
+                                  'aden_sales_445110')]
+sum(is.na(groceryData$count_sales_445110)) # = 0 !!!
+
+write.csv(groceryData,'grocery.csv')
---
+++ health/cleanHealth.R
@@ -0,0 +1,40 @@
+library("tidyverse")
+if (Sys.info()[1] == "Windows") {
+  setwd("C:/Users/peter/My Tresors/Documentsacer/KULeuven/CollAnaBigData/data/health")
+} else {
+  setwd("/home/muddy/Tresors/Documentsacer/KULeuven/CollAnaBigData/data/health")   
+}
+# https://chronicdata.cdc.gov/500-Cities-Places/PLACES-Local-Data-for-Better-Health-ZCTA-Data-2021/qnzd-25i4
+healthFile <- 'PLACES__Local_Data_for_Better_Health__ZCTA_Data_2021_release.csv'
+health <- read.csv(healthFile)
+# LocationName: 5 digits ZIP Code Tabulation Area code
+# LocationID: 5-digit Zip Code Tabulation Area (ZCTA5) code
+# Geolocation: Latitude, Longitude of city centroid (Format: Point(Longitude Latitude))
+# TotalPopulation: Total population of Census 2010 
+colnames(health)
+health <- select(health, -c( 'DataSource','Data_Value_Footnote_Symbol',  
+                             'Data_Value_Footnote','Low_Confidence_Limit', 
+                             'High_Confidence_Limit','DataValueTypeID'))
+# Measure == 'Obesity among adults aged >=18 years'
+levels(health$Measure)
+levels(health$MeasureId)
+# Obesity among adults aged >=18 years
+# Depression among adults aged >=18 years
+# Cancer (excluding skin cancer) among adults aged >=18 years
+# Cholesterol screening among adults aged >=18 years
+# Coronary heart disease among adults aged >=18 years
+healthFilter <- health %>% filter(MeasureId=='OBESITY' |
+                                  MeasureId=='DEPRESSION' |
+                                  MeasureId=='CANCER' |
+                                  MeasureId=='CHOLSCREEN' |
+                                  MeasureId=='CHD')
+
+healthwide <- pivot_wider(healthFilter, id_cols=c(Year,LocationName,Geolocation, TotalPopulation),
+                          names_from=c(MeasureId),values_from=Data_Value)
+# Warning message:
+# Values from `Data_Value` are not uniquely identified; output will contain list-cols.
+write.csv(healthwide,'health.csv')
+health %>%
+  dplyr::group_by(Short_Question_Text, MeasureId, Measure) %>%
+  dplyr::summarise(n = dplyr::n(), .groups = "drop") %>%
+  dplyr::filter(n > 1L) 
